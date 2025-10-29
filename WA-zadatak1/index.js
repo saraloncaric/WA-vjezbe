f03380bcbe5423f05fc53062894eb6e210850449
saraloncaric <167783165+saraloncaric@users.noreply.github.com>
2025-10-29 15:52:29 +0100

    dovrsetak zadatka

--- WA-zadatak1/index.js
+++ WA-zadatak1/index.js
@@ -1,16 +1,39 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 
+const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(PORT, error => {
-    if(error) {
-        console.error(`Greska prilikom pokretanja poslužitelja: ${error.message}`);
-    } else {
-        console.log(`Server je pokrenut na http://localhost:${PORT}`);
-    }
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/public/about.html')
+})
+
+app.get('/users', (req, res) => {
+    const users = [
+        { 
+            id: 1, 
+            ime: 'Sara', 
+            prezime: 'Lončarić'
+        },
+        { 
+            id: 2, 
+            ime: 'Ana', 
+            prezime: 'Anić' 
+        },
+        { 
+            id: 3, 
+            ime: 'Marija', 
+            prezime: 'Marić' 
+        },
+    ];
+    res.json(users);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server je pokrenut na http://localhost:${PORT}`);
+    
 });
